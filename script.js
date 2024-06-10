0beacae8a920affa203760ea6390803f2b86224a
flopeace <florian.tchomga@gmail.com>
2024-06-10 10:57:50 +0100

    push

---
+++ script.js
@@ -0,0 +1,131 @@
+const voeux = [
+    "Que cette nouvelle année soit remplie de bonheur, de succès et de moments inoubliables. Puisses-tu réaliser tous tes rêves et aspirations. Joyeux anniversaire !",
+    "Je te souhaite une santé de fer, une joie sans fin et une vie remplie de belles surprises. Que chaque jour soit une nouvelle aventure. Joyeux anniversaire !",
+    "Puisses-tu être entouré(e) de tes proches et de beaucoup d'amour en ce jour spécial. Que tes souhaits les plus chers se réalisent. Joyeux anniversaire !",
+    "En ce jour exceptionnel, je te souhaite une année fabuleuse pleine de découvertes, de réussite et de moments magiques. Joyeux anniversaire à toi !",
+    "Que cette année soit encore plus belle que la précédente, avec des sourires, des rires et des moments précieux partagés avec ceux que tu aimes. Joyeux anniversaire !",
+    "Je te souhaite une journée extraordinaire, remplie de surprises et de bonheur. Que cette nouvelle année de vie soit merveilleuse à tous points de vue. Joyeux anniversaire !",
+    "Puisses-tu toujours être entouré(e) de personnes aimantes et bienveillantes. Que tes jours soient ensoleillés et tes nuits étoilées. Joyeux anniversaire !",
+    "Que chaque moment de cette journée soit une douce mélodie dans ta mémoire. Je te souhaite beaucoup de bonheur et de succès dans tout ce que tu entreprendras. Joyeux anniversaire !",
+    "En ce jour spécial, que les étoiles brillent plus fort pour toi. Puisses-tu trouver la joie et l'amour dans chaque instant. Joyeux anniversaire !",
+    "Que cette journée soit remplie de doux souvenirs, de rires partagés et de cadeaux inattendus. Je te souhaite un anniversaire aussi merveilleux que toi !",
+    "Je te souhaite une année remplie de succès professionnels, de bonheur personnel et de moments inoubliables avec ceux que tu aimes. Joyeux anniversaire !",
+    "Puisses-tu continuer à briller et à inspirer les autres par ta bonté et ta sagesse. Que cette nouvelle année t'apporte tout ce que tu désires. Joyeux anniversaire !",
+    "Que cette journée soit aussi radieuse que ton sourire et aussi merveilleuse que ton âme. Je te souhaite un anniversaire rempli de joie et de surprises. Joyeux anniversaire !",
+    "Je te souhaite une année pleine de découvertes enrichissantes, de succès éclatants et de moments précieux partagés avec tes proches. Joyeux anniversaire !",
+    "Que chaque instant de cette journée soit un moment de bonheur et de joie. Je te souhaite tout le meilleur pour cette nouvelle année de vie. Joyeux anniversaire !",
+    "Puisses-tu toujours trouver la force et le courage de réaliser tes rêves. Que cette année soit pleine d'opportunités et de réussites. Joyeux anniversaire !",
+    "En ce jour spécial, je te souhaite une vie pleine de rires, de sourires et de moments inoubliables. Que tous tes souhaits se réalisent. Joyeux anniversaire !",
+    "Que cette nouvelle année de vie t'apporte autant de bonheur que tu en donnes aux autres. Je te souhaite une journée exceptionnelle et un joyeux anniversaire !",
+    "Puisses-tu être entouré(e) de tes proches et de beaucoup d'amour en ce jour spécial. Que tes souhaits les plus chers se réalisent. Joyeux anniversaire !",
+    "En ce jour exceptionnel, je te souhaite une année fabuleuse pleine de découvertes, de réussite et de moments magiques. Joyeux anniversaire à toi !",
+    "Que cette année soit encore plus belle que la précédente, avec des sourires, des rires et des moments précieux partagés avec ceux que tu aimes. Joyeux anniversaire !",
+    "Je te souhaite une journée extraordinaire, remplie de surprises et de bonheur. Que cette nouvelle année de vie soit merveilleuse à tous points de vue. Joyeux anniversaire !",
+    "Puisses-tu toujours être entouré(e) de personnes aimantes et bienveillantes. Que tes jours soient ensoleillés et tes nuits étoilées. Joyeux anniversaire !",
+    "Que chaque moment de cette journée soit une douce mélodie dans ta mémoire. Je te souhaite beaucoup de bonheur et de succès dans tout ce que tu entreprendras. Joyeux anniversaire !",
+    "En ce jour spécial, que les étoiles brillent plus fort pour toi. Puisses-tu trouver la joie et l'amour dans chaque instant. Joyeux anniversaire !",
+    "Que cette journée soit remplie de doux souvenirs, de rires partagés et de cadeaux inattendus. Je te souhaite un anniversaire aussi merveilleux que toi !",
+    "Je te souhaite une année remplie de succès professionnels, de bonheur personnel et de moments inoubliables avec ceux que tu aimes. Joyeux anniversaire !",
+    "Puisses-tu continuer à briller et à inspirer les autres par ta bonté et ta sagesse. Que cette nouvelle année t'apporte tout ce que tu désires. Joyeux anniversaire !",
+    "Que cette journée soit aussi radieuse que ton sourire et aussi merveilleuse que ton âme. Je te souhaite un anniversaire rempli de joie et de surprises. Joyeux anniversaire !",
+    "Je te souhaite une année pleine de découvertes enrichissantes, de succès éclatants et de moments précieux partagés avec tes proches. Joyeux anniversaire !",
+    "Que chaque instant de cette journée soit un moment de bonheur et de joie. Je te souhaite tout le meilleur pour cette nouvelle année de vie. Joyeux anniversaire !",
+    "Puisses-tu toujours trouver la force et le courage de réaliser tes rêves. Que cette année soit pleine d'opportunités et de réussites. Joyeux anniversaire !",
+    "En ce jour spécial, je te souhaite une vie pleine de rires, de sourires et de moments inoubliables. Que tous tes souhaits se réalisent. Joyeux anniversaire !",
+    "Que cette nouvelle année de vie t'apporte autant de bonheur que tu en donnes aux autres. Je te souhaite une journée exceptionnelle et un joyeux anniversaire !",
+    "Puisses-tu être entouré(e) de tes proches et de beaucoup d'amour en ce jour spécial. Que tes souhaits les plus chers se réalisent. Joyeux anniversaire !",
+    "En ce jour exceptionnel, je te souhaite une année fabuleuse pleine de découvertes, de réussite et de moments magiques. Joyeux anniversaire à toi !",
+    "Que cette année soit encore plus belle que la précédente, avec des sourires, des rires et des moments précieux partagés avec ceux que tu aimes. Joyeux anniversaire !",
+    "Je te souhaite une journée extraordinaire, remplie de surprises et de bonheur. Que cette nouvelle année de vie soit merveilleuse à tous points de vue. Joyeux anniversaire !",
+    "Puisses-tu toujours être entouré(e) de personnes aimantes et bienveillantes. Que tes jours soient ensoleillés et tes nuits étoilées. Joyeux anniversaire !",
+    "Que chaque moment de cette journée soit une douce mélodie dans ta mémoire. Je te souhaite beaucoup de bonheur et de succès dans tout ce que tu entreprendras. Joyeux anniversaire !",
+    "En ce jour spécial, que les étoiles brillent plus fort pour toi. Puisses-tu trouver la joie et l'amour dans chaque instant. Joyeux anniversaire !",
+    "Que cette journée soit remplie de doux souvenirs, de rires partagés et de cadeaux inattendus. Je te souhaite un anniversaire aussi merveilleux que toi !",
+    "Je te souhaite une année remplie de succès professionnels, de bonheur personnel et de moments inoubliables avec ceux que tu aimes. Joyeux anniversaire !",
+    "Puisses-tu continuer à briller et à inspirer les autres par ta bonté et ta sagesse. Que cette nouvelle année t'apporte tout ce que tu désires. Joyeux anniversaire !",
+    "Que cette journée soit aussi radieuse que ton sourire et aussi merveilleuse que ton âme. Je te souhaite un anniversaire rempli de joie et de surprises. Joyeux anniversaire !",
+    "Je te souhaite une année pleine de découvertes enrichissantes, de succès éclatants et de moments précieux partagés avec tes proches. Joyeux anniversaire !",
+    "Que chaque instant de cette journée soit un moment de bonheur et de joie. Je te souhaite tout le meilleur pour cette nouvelle année de vie. Joyeux anniversaire !",
+    "Puisses-tu toujours trouver la force et le courage de réaliser tes rêves. Que cette année soit pleine d'opportunités et de réussites. Joyeux anniversaire !",
+    "En ce jour spécial, je te souhaite une vie pleine de rires, de sourires et de moments inoubliables. Que tous tes souhaits se réalisent. Joyeux anniversaire !",
+    "Que cette nouvelle année de vie t'apporte autant de bonheur que tu en donnes aux autres. Je te souhaite une journée exceptionnelle et un joyeux anniversaire !",
+    "Puisses-tu être entouré(e) de tes proches et de beaucoup d'amour en ce jour spécial. Que tes souhaits les plus chers se réalisent. Joyeux anniversaire !",
+    "En ce jour exceptionnel, je te souhaite une année fabuleuse pleine de découvertes, de réussite et de moments magiques. Joyeux anniversaire à toi !",
+    "Que cette année soit encore plus belle que la précédente, avec des sourires, des rires et des moments précieux partagés avec ceux que tu aimes. Joyeux anniversaire !",
+    "Je te souhaite une journée extraordinaire, remplie de surprises et de bonheur. Que cette nouvelle année de vie soit merveilleuse à tous points de vue. Joyeux anniversaire !",
+    "Puisses-tu toujours être entouré(e) de personnes aimantes et bienveillantes. Que tes jours soient ensoleillés et tes nuits étoilées. Joyeux anniversaire !",
+    "Que chaque moment de cette journée soit une douce mélodie dans ta mémoire. Je te souhaite beaucoup de bonheur et de succès dans tout ce que tu entreprendras. Joyeux anniversaire !",
+    "En ce jour spécial, que les étoiles brillent plus fort pour toi. Puisses-tu trouver la joie et l'amour dans chaque instant. Joyeux anniversaire !",
+    "Que cette journée soit remplie de doux souvenirs, de rires partagés et de cadeaux inattendus. Je te souhaite un anniversaire aussi merveilleux que toi !",
+    "Je te souhaite une année remplie de succès professionnels, de bonheur personnel et de moments inoubliables avec ceux que tu aimes. Joyeux anniversaire !",
+    "Puisses-tu continuer à briller et à inspirer les autres par ta bonté et ta sagesse. Que cette nouvelle année t'apporte tout ce que tu désires. Joyeux anniversaire !",
+    "Que cette journée soit aussi radieuse que ton sourire et aussi merveilleuse que ton âme. Je te souhaite un anniversaire rempli de joie et de surprises. Joyeux anniversaire !",
+    "Je te souhaite une année pleine de découvertes enrichissantes, de succès éclatants et de moments précieux partagés avec tes proches. Joyeux anniversaire !",
+    "Que chaque instant de cette journée soit un moment de bonheur et de joie. Je te souhaite tout le meilleur pour cette nouvelle année de vie. Joyeux anniversaire !",
+    "Puisses-tu toujours trouver la force et le courage de réaliser tes rêves. Que cette année soit pleine d'opportunités et de réussites. Joyeux anniversaire !",
+    "En ce jour spécial, je te souhaite une vie pleine de rires, de sourires et de moments inoubliables. Que tous tes souhaits se réalisent. Joyeux anniversaire !",
+    "Que cette nouvelle année de vie t'apporte autant de bonheur que tu en donnes aux autres. Je te souhaite une journée exceptionnelle et un joyeux anniversaire !",
+    "Puisses-tu être entouré(e) de tes proches et de beaucoup d'amour en ce jour spécial. Que tes souhaits les plus chers se réalisent. Joyeux anniversaire !",
+    "En ce jour exceptionnel, je te souhaite une année fabuleuse pleine de découvertes, de réussite et de moments magiques. Joyeux anniversaire à toi !",
+    "Que cette année soit encore plus belle que la précédente, avec des sourires, des rires et des moments précieux partagés avec ceux que tu aimes. Joyeux anniversaire !",
+    "Je te souhaite une journée extraordinaire, remplie de surprises et de bonheur. Que cette nouvelle année de vie soit merveilleuse à tous points de vue. Joyeux anniversaire !",
+    "Puisses-tu toujours être entouré(e) de personnes aimantes et bienveillantes. Que tes jours soient ensoleillés et tes nuits étoilées. Joyeux anniversaire !",
+    "Que chaque moment de cette journée soit une douce mélodie dans ta mémoire. Je te souhaite beaucoup de bonheur et de succès dans tout ce que tu entreprendras. Joyeux anniversaire !",
+    "En ce jour spécial, que les étoiles brillent plus fort pour toi. Puisses-tu trouver la joie et l'amour dans chaque instant. Joyeux anniversaire !",
+    "Que cette journée soit remplie de doux souvenirs, de rires partagés et de cadeaux inattendus. Je te souhaite un anniversaire aussi merveilleux que toi !",
+    "Je te souhaite une année remplie de succès professionnels, de bonheur personnel et de moments inoubliables avec ceux que tu aimes. Joyeux anniversaire !",
+    "Puisses-tu continuer à briller et à inspirer les autres par ta bonté et ta sagesse. Que cette nouvelle année t'apporte tout ce que tu désires. Joyeux anniversaire !",
+    "Que cette journée soit aussi radieuse que ton sourire et aussi merveilleuse que ton âme. Je te souhaite un anniversaire rempli de joie et de surprises. Joyeux anniversaire !",
+    "Je te souhaite une année pleine de découvertes enrichissantes, de succès éclatants et de moments précieux partagés avec tes proches. Joyeux anniversaire !",
+    "Que chaque instant de cette journée soit un moment de bonheur et de joie. Je te souhaite tout le meilleur pour cette nouvelle année de vie. Joyeux anniversaire !",
+    "Puisses-tu toujours trouver la force et le courage de réaliser tes rêves. Que cette année soit pleine d'opportunités et de réussites. Joyeux anniversaire !",
+    "En ce jour spécial, je te souhaite une vie pleine de rires, de sourires et de moments inoubliables. Que tous tes souhaits se réalisent. Joyeux anniversaire !",
+    "Que cette nouvelle année de vie t'apporte autant de bonheur que tu en donnes aux autres. Je te souhaite une journée exceptionnelle et un joyeux anniversaire !",
+    "Puisses-tu être entouré(e) de tes proches et de beaucoup d'amour en ce jour spécial. Que tes souhaits les plus chers se réalisent. Joyeux anniversaire !",
+    "En ce jour exceptionnel, je te souhaite une année fabuleuse pleine de découvertes, de réussite et de moments magiques. Joyeux anniversaire à toi !",
+    "Que cette année soit encore plus belle que la précédente, avec des sourires, des rires et des moments précieux partagés avec ceux que tu aimes. Joyeux anniversaire !",
+    "Je te souhaite une journée extraordinaire, remplie de surprises et de bonheur. Que cette nouvelle année de vie soit merveilleuse à tous points de vue. Joyeux anniversaire !",
+    "Puisses-tu toujours être entouré(e) de personnes aimantes et bienveillantes. Que tes jours soient ensoleillés et tes nuits étoilées. Joyeux anniversaire !",
+    "Que chaque moment de cette journée soit une douce mélodie dans ta mémoire. Je te souhaite beaucoup de bonheur et de succès dans tout ce que tu entreprendras. Joyeux anniversaire !",
+    "En ce jour spécial, que les étoiles brillent plus fort pour toi. Puisses-tu trouver la joie et l'amour dans chaque instant. Joyeux anniversaire !",
+    "Que cette journée soit remplie de doux souvenirs, de rires partagés et de cadeaux inattendus. Je te souhaite un anniversaire aussi merveilleux que toi !",
+    "Je te souhaite une année remplie de succès professionnels, de bonheur personnel et de moments inoubliables avec ceux que tu aimes. Joyeux anniversaire !",
+    "Puisses-tu continuer à briller et à inspirer les autres par ta bonté et ta sagesse. Que cette nouvelle année t'apporte tout ce que tu désires. Joyeux anniversaire !",
+    "Que cette journée soit aussi radieuse que ton sourire et aussi merveilleuse que ton âme. Je te souhaite un anniversaire rempli de joie et de surprises. Joyeux anniversaire !",
+    "Je te souhaite une année pleine de découvertes enrichissantes, de succès éclatants et de moments précieux partagés avec tes proches. Joyeux anniversaire !",
+    "Que chaque instant de cette journée soit un moment de bonheur et de joie. Je te souhaite tout le meilleur pour cette nouvelle année de vie. Joyeux anniversaire !",
+    "Puisses-tu toujours trouver la force et le courage de réaliser tes rêves. Que cette année soit pleine d'opportunités et de réussites. Joyeux anniversaire !",
+    "En ce jour spécial, je te souhaite une vie pleine de rires, de sourires et de moments inoubliables. Que tous tes souhaits se réalisent. Joyeux anniversaire !"
+];
+
+
+function afficherVoeux() {
+    const voeuxElement = document.getElementById('voeux');
+    const voeuxAleatoire = voeux[Math.floor(Math.random() * voeux.length)];
+
+    gsap.to(voeuxElement, {duration: 0.5, opacity: 0, onComplete: () => {
+        voeuxElement.textContent = voeuxAleatoire;
+        gsap.to(voeuxElement, {duration: 0.5, opacity: 1});
+    }});
+}
+
+// Diaporama
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slideshow .img');
+
+function showSlide(index) {
+    gsap.to(slides[currentSlide], {duration: 1, opacity: 0});
+    currentSlide = index;
+    gsap.to(slides[currentSlide], {duration: 1, opacity: 1});
+}
+
+function nextSlide() {
+    const nextIndex = (currentSlide + 1) % slides.length;
+    showSlide(nextIndex);
+    afficherVoeux();
+}
+
+// Initialisation
+afficherVoeux();
+gsap.to(slides[0], {duration: 1, opacity: 1});
+setInterval(nextSlide, 3000); // Changer d'image toutes les 3 secondes
